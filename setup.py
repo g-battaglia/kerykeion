928e1a55eaa0fbd57f297ab660cf7495e88fd1eb
g-battaglia <kerykeion.astrology@gmail.com>
2020-08-03 03:20:11 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kerykeion", # Replace with your own username
-    version="0.0.1",
+    version="0.0.1.1",
     author="Giacomo Battaglia",
     author_email="battaglia.giacomo@yahoo.it",
     description="A astrology library.",
