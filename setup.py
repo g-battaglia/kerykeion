c9b1fc03824d44429c96bdc97f6dff75e9ad8976
g-battaglia <battaglia.giacomo@icloud.com>
2021-02-17 21:34:49 +0100

    Versione update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kerykeion", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="Giacomo Battaglia",
     author_email="battaglia.giacomo@yahoo.it",
     description="A astrology library.",
