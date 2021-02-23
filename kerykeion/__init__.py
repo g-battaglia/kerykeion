2053dbc611d36b510a814f4d4f603e5a0bb817c9
g-battaglia <kerykeion.astrology@gmail.com>
2021-02-23 15:09:53 +0100

    Update

--- kerykeion/__init__.py
+++ kerykeion/__init__.py
@@ -1,7 +1,6 @@
 import sys
 from os.path import dirname
 from kerykeion.astrocore import AstroData, Calculator
-from kerykeion.chart_svg import SvgInstance
 from kerykeion.output_exemple import output
 
 sys.path.append(dirname(__file__))
