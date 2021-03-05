6ec7d0ae27c4e35a17ecb9147a81be4fd315182e
g-battaglia <battaglia.giacomo@icloud.com>
2021-03-06 00:12:58 +0100

    Fix

--- kerykeion/__init__.py
+++ kerykeion/__init__.py
@@ -2,5 +2,6 @@ import sys
 from os.path import dirname
 from kerykeion.astrocore import AstroData, Calculator
 from kerykeion.output_exemple import output
+from kerykeion.utilities import kr_settings as settings
 
 sys.path.append(dirname(__file__))
---
+++ kerykeion/utilities/__init__.py
@@ -0,0 +1,1 @@
+from kerykeion.utilities.aspects import NatalAspects
