abdbb3a9d69dd98e750d95e0aad28feca3a0d76e
g-battaglia <kerykeion.astrology@gmail.com>
2021-03-07 23:45:05 +0100

    Same

--- kerykeion/__init__.py
+++ kerykeion/__init__.py
@@ -3,5 +3,6 @@ from os.path import dirname
 from kerykeion.astrocore import AstroData, Calculator
 from kerykeion.output_exemple import output
 from kerykeion.utilities import kr_settings as settings
+from kerykeion.utilities.general import print_settings_path
 
 sys.path.append(dirname(__file__))
