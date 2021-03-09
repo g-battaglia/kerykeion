a65d9266b500c7a4f2dde99d56c1af3edacfabee
g-battaglia <battaglia.giacomo@icloud.com>
2021-03-09 23:23:52 +0100

    MakeSvgInstance fully implemented to generate SVG charts.

--- kerykeion/__init__.py
+++ kerykeion/__init__.py
@@ -4,5 +4,6 @@ from kerykeion.astrocore import AstroData, Calculator
 from kerykeion.output_exemple import output
 from kerykeion.utilities import kr_settings as settings
 from kerykeion.utilities.general import print_settings_path
+from kerykeion.utilities.charts import MakeSvgInstance as MakeSvgInstance
 
 sys.path.append(dirname(__file__))
