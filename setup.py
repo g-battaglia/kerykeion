63d23a5d93e2fb2bb5b4e36cfb8c54436a793a92
g-battaglia <kerykeion.astrology@gmail.com>
2021-12-22 21:19:18 +0100

    Versione update for pip

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="kerykeion",
-    version="2.1.15",
+    version="2.1.16",
     author="Giacomo Battaglia",
     author_email="battaglia.giacomo@yahoo.it",
     description="An astrology library.",
