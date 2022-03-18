37e2227216fa360124d23ba5b8d5620ccf715ff3
Konstantin Gavrilov <gavrilovks@gmail.com>
2022-03-17 22:03:15 -0700

    change encoding to utf-8 for output file

--- kerykeion/utilities/charts/charts_svg.py
+++ kerykeion/utilities/charts/charts_svg.py
@@ -509,7 +509,7 @@ class MakeSvgInstance:
         self.chartname = os.path.join(
             self.output_directory, f'{self.name}{self.type}Chart.svg')
 
-        with open(self.chartname, "w") as output_file:
+        with open(self.chartname, "w", encoding='utf-8') as output_file:
             output_file.write(self.template)
 
         return print(f"SVG Generated Correctly in: {self.output_directory}")
