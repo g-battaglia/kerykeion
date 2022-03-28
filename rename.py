56b587b3b72b066c351037eae470701cfab39f14
g-battaglia <battaglia.giacomo@icloud.com>
2022-03-28 18:14:55 +0200

    Rename reademe

---
+++ rename.py
@@ -0,0 +1,28 @@
+"""
+    This is part of Kerykeion (C) 2020 Giacomo Battaglia
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kerykeion",
+    version="2.2.4",
+    author="Giacomo Battaglia",
+    author_email="battaglia.giacomo@yahoo.it",
+    description="A python library for astrology.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/g-battaglia/kerykeion",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=['pyswisseph', 'pytz', 'jsonpickle'],
+)
