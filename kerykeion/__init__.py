4791716afab9c1bdf62892e9e2752de1a3f9aad8
g-battaglia <kerykeion.astrology@gmail.com>
2022-04-02 16:29:04 +0200

    Created an exemple directory for expemple scripts. Also changed the structure of the config files, now there is a config file in the JSON format that can be given as a kwarg when you create a chart. TODO: Refactoring Aspect class.

--- kerykeion/__init__.py
+++ kerykeion/__init__.py
@@ -2,8 +2,6 @@ import sys
 from os.path import dirname
 from kerykeion.main import KrInstance
 from kerykeion.output_exemple import output
-from kerykeion.utilities import kr_settings as settings
-from kerykeion.utilities.general import print_settings_path
 from kerykeion.utilities.charts import MakeSvgInstance as MakeSvgInstance
 from kerykeion.utilities.discepolo import DiscepoloNumber
 
