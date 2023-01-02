279481ac6d41dd8385fa84b059ae13ec8b0e09bb
Giacomo Battaglia <giacomobattaglia@Air-di-Giacomo.wind3.hub>
2023-01-03 00:58:06 +0100

    Add gh action

--- kerykeion/aspects/__init__.py
+++ kerykeion/aspects/__init__.py
@@ -1,2 +1,7 @@
+"""
+The aspects module contains the classes and functions for calculating 
+ aspects between planets and points in a chart.
+"""
+
 from .composite_aspects import CompositeAspects
 from .natal_aspects import NatalAspects
