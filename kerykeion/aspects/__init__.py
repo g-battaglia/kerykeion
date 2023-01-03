566e5fe9355e901d0eb624a54ebaf79c76bac3ae
Giacomo Battaglia <giacomobattaglia@Air-di-Giacomo.wind3.hub>
2023-01-03 01:13:38 +0100

    Updates

--- kerykeion/aspects/__init__.py
+++ kerykeion/aspects/__init__.py
@@ -1,7 +1,11 @@
 """
+This is part of Kerykeion (C) 2023 Giacomo Battaglia
+
 The aspects module contains the classes and functions for calculating 
  aspects between planets and points in a chart.
 """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from .composite_aspects import CompositeAspects
 from .natal_aspects import NatalAspects
