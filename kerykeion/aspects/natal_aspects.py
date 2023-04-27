e4ca4c53898f5515868bc9e3515e759877a1e570
g-battaglia <battaglia.giacomo@icloud.com>
2023-04-28 00:24:07 +0200

    Still refactory

--- kerykeion/aspects/natal_aspects.py
+++ kerykeion/aspects/natal_aspects.py
@@ -206,7 +206,7 @@ class NatalAspects:
 
         set_points_name = []
         for p in self.planets_settings:
-            if p["active"]:
+            if p["is_active"]:
                 set_points_name.append(p["name"])
 
         point_list = []
@@ -268,7 +268,7 @@ class NatalAspects:
 
         aspects_filtered = []
         for a in self.all_aspects_list:
-            if self.aspects_settings[a["aid"]]["active"] == True:
+            if self.aspects_settings[a["aid"]]["is_active"] == True:
                 aspects_filtered.append(a)
 
         axes_list = [
