c87673f66ed6e87748e0185bdda62f260a806c48
g-battaglia <battaglia.giacomo@icloud.com>
2024-08-27 14:14:49 +0200

    clean up

--- kerykeion/aspects/aspects_utils.py
+++ kerykeion/aspects/aspects_utils.py
@@ -12,15 +12,19 @@ from kerykeion.kr_types.kr_models import AstrologicalSubjectModel
 from kerykeion.kr_types.settings_models import KerykeionSettingsCelestialPointModel, KerykeionSettingsAspectModel
 
 
-def get_aspect_from_two_points(aspects_settings: Union[list[KerykeionSettingsAspectModel], list[dict]], point_one: Union[float, int], point_two: Union[float, int]):
+def get_aspect_from_two_points(
+    aspects_settings: Union[list[KerykeionSettingsAspectModel], list[dict]],
+    point_one: Union[float, int],
+    point_two: Union[float, int],
+):
     """
     Utility function to calculate the aspects between two points.
-    
+
     Args:
         aspects_settings (dict): Dictionary containing aspect settings.
         point_one (Union[float, int]): First point.
         point_two (Union[float, int]): Second point.
-    
+
     Returns:
         dict: Dictionary containing the aspect details.
     """
@@ -49,7 +53,7 @@ def get_aspect_from_two_points(aspects_settings: Union[list[KerykeionSettingsAsp
         "distance": distance - aspect_degrees,
         "aspect_degrees": aspect_degrees,
         "aid": aid,
-        "diff": diff
+        "diff": diff,
     }
 
 
@@ -67,7 +71,9 @@ def planet_id_decoder(planets_settings: list[KerykeionSettingsCelestialPointMode
     raise ValueError(f"Planet {name} not found in the settings")
 
 
-def get_active_points_list(subject: Union[AstrologicalSubject, AstrologicalSubjectModel], settings: Union[KerykeionSettingsModel, dict]) -> list:
+def get_active_points_list(
+    subject: Union[AstrologicalSubject, AstrologicalSubjectModel], settings: Union[KerykeionSettingsModel, dict]
+) -> list:
     """
     Given an astrological subject and the settings, return a list of the active points.
     Args:
