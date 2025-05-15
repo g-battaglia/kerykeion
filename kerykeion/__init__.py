4a48ca773553ad9a0b8c7e87620029e72df52d08
g-battaglia <battaglia.giacomo@icloud.com>
2025-05-15 14:23:58 +0200

    house_comparison: WIP

--- kerykeion/__init__.py
+++ kerykeion/__init__.py
@@ -6,15 +6,16 @@ This is part of Kerykeion (C) 2025 Giacomo Battaglia
 """
 
 # Local
+from .aspects import SynastryAspects, NatalAspects
 from .astrological_subject import AstrologicalSubject
 from .charts.kerykeion_chart_svg import KerykeionChartSVG
+from .composite_subject_factory import CompositeSubjectFactory
+from .enums import Planets, Aspects, Signs
+from .ephemeris_data import EphemerisDataFactory
+from .house_comparison import HouseComparisonFactory, HouseComparisonModel, PointInHouseModel
 from .kr_types import *
 from .relationship_score.relationship_score import RelationshipScore
 from .relationship_score.relationship_score_factory import RelationshipScoreFactory
-from .aspects import SynastryAspects, NatalAspects
 from .report import Report
 from .settings import KerykeionSettingsModel, get_settings
-from .enums import Planets, Aspects, Signs
-from .ephemeris_data import EphemerisDataFactory
-from .composite_subject_factory import CompositeSubjectFactory
 from .transits_time_range import TransitsTimeRangeFactory
