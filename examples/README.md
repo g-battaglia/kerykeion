df0b410b6be15eb401de35c99892e884c98fa925
g-battaglia <kerykeion.astrology@gmail.com>
2025-08-15 23:55:07 +0200

    removed "examples" folder

--- examples/README.md
+++
@@ -1,4 +0,0 @@
-# Examples
-
-In this folder are contained some examples to understand how to use the library.
-The same file are used to generate the unit tests.
--- examples/charts/kerykeion_chart_svg.py
+++
@@ -1,7 +0,0 @@
-from kerykeion import AstrologicalSubjectFactory
-from kerykeion import KerykeionChartSVG
-
-first = AstrologicalSubjectFactory.from_birth_data("John Lennon", 1940, 10, 9, 18, 30, "Liverpool", "GB")
-second = AstrologicalSubjectFactory.from_birth_data("Paul McCartney", 1942, 6, 18, 15, 30, "Liverpool", "GB")
-
-chart = KerykeionChartSVG(first, "Synastry", second)
